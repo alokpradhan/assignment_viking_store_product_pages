207c176c51649966292bb61cdc9f8dcdec4c00d3
alokp <alokpra@gmail.com>
2015-07-31 14:14:56 -0700

    working on orders

--- app/controllers/order_contents_controller.rb
+++ app/controllers/order_contents_controller.rb
@@ -3,7 +3,17 @@ class OrderContentsController < ApplicationController
   def destroy
     OrderContent.find(params[:id]).destroy
     redirect_to URI(request.referer).path
-
   end
+
+  def update
+  	OrderContent.find(params[:id]).update(whitelisted_params)
+  	redirect_to URI(request.referer).path
+  end
+
+private
+
+	def whitelisted_params
+		params.require(:order_content).permit(:quantity)
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
       resources :products
       resources :categories
       resources :addresses
-      resources :order_contents, :only => [:destroy]
+      resources :order_contents, :only => [:destroy, :update]
       resources :orders, :except => [:new, :create]
       resources :users do
         resources :addresses #:only => [:index, :new, :create]
